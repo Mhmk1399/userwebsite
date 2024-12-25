77ca386dd0eec994cdf9756f0168ab205cd51a7c
mohamadhosein <mhmk1399@gmail.com>
2024-12-25 18:55:40 +0330

    orders

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -2,26 +2,26 @@ import connect from "@/lib/data";
 import Order from "@/models/orders";
 import { NextResponse } from "next/server";
 
-
-
-
-
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  try {
+    const id = await context.params.id;
     await connect();
+
     if (!connect) {
-        return NextResponse.json({ error: "Connection failed!" });
+      return NextResponse.json({ error: "Connection failed!" });
     }
-    try {
-        const id = params.id;
-        const order = await Order.findById(id);
-        if (!order) {
-            return NextResponse.json({ error: "Order not found" }, { status: 404 });
-        }
-        return NextResponse.json(order, { status: 200 });
+
+    const order = await Order.findById(id);
+    if (!order) {
+      return NextResponse.json({ error: "Order not found" }, { status: 404 });
     }
-    catch (error) {
-        console.error("Error fetching order:", error);
-        return NextResponse.json({ error: "Failed to fetch order" }, { status: 500 });
-    }
+
+    return NextResponse.json(order, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching order:", error);
+    return NextResponse.json({ error: "Failed to fetch order" }, { status: 500 });
+  }
 }
--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,17 +1,31 @@
 import { NextResponse } from 'next/server'
 import connect from '@/lib/data'
-import  Order  from '@/models/orders'
+import Order from '@/models/orders'
 
 
-export async function POST (req: Request) {
-    await connect();
+export async function POST(req: Request) {
+  await connect();
+  if (!connect)
+    return NextResponse.json({ error: "Database connection error" }, { status: 500 })
+  try {
     const body = await req.json()
     const order = new Order(body)
     await order.save()
-    
-
-
-
-
-    return new Response('Hello, Next.js!')
+    return NextResponse.json({ message: "Order created successfully" }, { status: 201 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 })
   }
+}
+export async function GET(req: Request) {
+  await connect();
+  if (!connect)
+    return NextResponse.json({ error: "Database connection error" }, { status: 500 })
+  try {
+    const orders = await Order.find()
+    return NextResponse.json(orders, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 })
+  }
+}
